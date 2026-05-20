400dfb5b573bc81ebc63b26b535339fcec95ffac
fransico08 <hmtlqd249@gmail.com>
2026-05-20 08:01:39 +0700

    feat: integrate frontend dev5, fix docker setup, add routes, fix nginx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './Pages/Dashboard'
+import LeaveRequestPage from './Pages/LeaveRequestPage'
+import LoginPage from './Pages/LoginPage'
+import Profile from './Pages/Profile'
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<Dashboard />} />
+        <Route path="/leave-request" element={<LeaveRequestPage />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   )
 }
 
 export default App
-
-
-
